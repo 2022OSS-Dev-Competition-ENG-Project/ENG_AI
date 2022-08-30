a55c191355496350e1b9010a50976da94e3f9449
Hyeontaemin <taemin14215@naver.com>
2022-08-30 17:09:19 +0900

    [CHORE] 지역 설정 다시

--- modelrun.py
+++ modelrun.py
@@ -30,9 +30,12 @@ def crolling(region):
     return result
 
 
-@app.route("/", methods=["POST"])
+@app.route("/", methods=['POST'])
 def predict():
-    region = "대구광역시 달서구 날씨"
+    params = request.get_json()
+    region = params["facilityAddress"]
+    region = region.split()
+    region = region[0] + region[1] + region[2]
     month = date.today().month
     result = crolling(region)
     result = np.array(result)
@@ -70,4 +73,4 @@ def predict():
     return str(message)
 
 
-app.run(host="0.0.0.0",port=2222)
+app.run(host="0.0.0.0", port=2222)
