ec0ac590332734b1d1394a84a2e1a5b069b089ed
Hyeontaemin <taemin14215@naver.com>
2022-09-13 15:25:04 +0900

    [ADD] 화재예측모델

--- randomforest.py
+++ randomforest.py
@@ -12,20 +12,20 @@ import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 
-data = pd.read_excel('/content/화재 7-9.xlsx')
+data = pd.read_excel('/content/화재 7-9.xlsx') # 엑셀 데이터 읽기
 
-ydata = data["위험도"].values
+ydata = data["위험도"].values # 정답 저장
 
-xdata = []
+xdata = [] # 온도와 습도를 저장하기위한 리스트 선언
 
 for i, rows in data.iterrows():
-    xdata.append([rows['평균기온'],rows['상대습도']])
-X_train, X_test, y_train, y_test = train_test_split(xdata,ydata,test_size=0.2, random_state=42)
+    xdata.append([rows['평균기온'],rows['상대습도']]) # 평균기온과 상대습도 컬럼 추
+X_train, X_test, y_train, y_test = train_test_split(xdata,ydata,test_size=0.2, random_state=42) # 데이터 셋 훈련셋 80퍼 테스트셋 20퍼로 분리
 
 
-rf_model = RandomForestClassifier(n_estimators=10, max_depth=5, random_state=0)
-rf_model = rf_model.fit(X_train,y_train)
-joblib.dump(rf_model, './randomforest7.pkl')
+rf_model = RandomForestClassifier(n_estimators=10, max_depth=5, random_state=0 # 입력해야함 
+rf_model = rf_model.fit(X_train,y_train) # 모델 학습
+joblib.dump(rf_model, './randomforest7.pkl') #모델 저장
 
-pred = rf_model.predict(X_test)
-print('Accuracy: %.2f' % accuracy_score(y_test, pred))
+pred = rf_model.predict(X_test) # 테스트셋 검증
+print('Accuracy: %.2f' % accuracy_score(y_test, pred)) # 정활도 출력
