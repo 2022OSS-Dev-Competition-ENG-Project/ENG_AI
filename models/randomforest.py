df8000429fce0fea289b31768f3a3a3a803086c6
Hyeontaemin <taemin14215@naver.com>
2022-09-13 18:18:33 +0900

    [CHORE]  주석 1차 정리

--- models/randomforest.py
+++ models/randomforest.py
@@ -19,11 +19,11 @@ ydata = data["위험도"].values # 정답 저장
 xdata = [] # 온도와 습도를 저장하기위한 리스트 선언
 
 for i, rows in data.iterrows():
-    xdata.append([rows['평균기온'],rows['상대습도']]) # 평균기온과 상대습도 컬럼 추
+    xdata.append([rows['평균기온'],rows['상대습도']]) # 평균기온과 상대습도 컬럼 추가
 X_train, X_test, y_train, y_test = train_test_split(xdata,ydata,test_size=0.2, random_state=42) # 데이터 셋 훈련셋 80퍼 테스트셋 20퍼로 분리
 
 
-rf_model = RandomForestClassifier(n_estimators=10, max_depth=5, random_state=0 # 입력해야함 
+rf_model = RandomForestClassifier(n_estimators=10, max_depth=5, random_state=0) # 결정트리 개수 = 10, 트리의 최대 깊이 = 5, random_state 설정
 rf_model = rf_model.fit(X_train,y_train) # 모델 학습
 joblib.dump(rf_model, './randomforest7.pkl') #모델 저장
 
